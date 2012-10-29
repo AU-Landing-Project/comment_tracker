bf609bc4539eb1c9562bfe2eeeb9674076dd4558
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2012-10-29 15:21:35 +0200

    Pass parameters to elgg_echo() instead of using sprintf. (Also add some comments and fix bad tab spacings.)

--- lib/functions.php
+++ lib/functions.php
@@ -1,41 +1,52 @@
 <?php
 
-/*
+/**
  * Returns bool - whether the user is subscribed or not
  */
 function comment_tracker_is_subscribed($user, $entity) {
-  if (!$user || !elgg_instanceof($user, 'user')) {
-    return false;
-  }
+	if (!$user || !elgg_instanceof($user, 'user')) {
+		return false;
+	}
   
-  if (!$entity || !($entity instanceof ElggEntity)) {
-    return false;
-  }
+	if (!$entity || !($entity instanceof ElggEntity)) {
+		return false;
+	}
   
-  $result = check_entity_relationship($user->guid, 'comment_subscribe', $entity->guid);
+	$result = check_entity_relationship($user->guid, 'comment_subscribe', $entity->guid);
   
-  $params = array('user' => $user, 'entity' => $entity);
+	$params = array('user' => $user, 'entity' => $entity);
   
-  // allow other plugins to affect the behaviour
-  return elgg_trigger_plugin_hook('subscription_check', 'comment_tracker', $params, $result);
+	// allow other plugins to affect the behaviour
+	return elgg_trigger_plugin_hook('subscription_check', 'comment_tracker', $params, $result);
 }
 
-
+/**
+ * Notify subscribed users with their preferred notification methods
+ */
 function comment_tracker_notify($annotation, $ann_user, $params = array()) {
 	global $NOTIFICATION_HANDLERS, $CONFIG;
 	
 	if (!($annotation instanceof ElggAnnotation)) {
-    return false;
+    	return false;
 	}
   
-  $entity = get_entity($annotation->entity_guid);
+	$entity = get_entity($annotation->entity_guid);
   
 	if ($entity instanceof ElggObject)	{
 		$container = get_entity($entity->container_guid);
-		$entity_link = $entity->getUrl();
 		$group_lang = ($entity->getSubtype() == 'groupforumtopic') ? "group:" : '';
 		
-    $subject = sprintf(elgg_echo("comment:notify:{$group_lang}subject"), $entity->title);
+		$entity_link = elgg_view('output/url', array(
+			'url' => $entity->getUrl(),
+			'text' => $entity->title,
+		));
+		
+		$commenter_link = elgg_view('output/url', array(
+			'url' => $ann_user->getUrl(),
+			'text' => $ann_user->name,
+		));
+		
+	 	$subject = sprintf(elgg_echo("comment:notify:{$group_lang}subject"), $entity->title);
 		
 		$options = array('relationship' => COMMENT_TRACKER_RELATIONSHIP,
 						 'relationship_guid' => $annotation->entity_guid,
@@ -47,17 +58,16 @@ function comment_tracker_notify($annotation, $ann_user, $params = array()) {
 		
 		$result = array();
 		foreach ($users as $user) {
-			if ($user instanceof ElggUser    // make sure user is real
-			    && $user->guid               // make sure user has a valid id
-			    && $user->guid != $ann_user->guid)    // no point notifying the author of comment
-			{
+			// Make sure user is real
+			// Do not notify the author of comment
+			if ($user instanceof ElggUser && $user->guid != $ann_user->guid) {
                 
 			    if ($user->guid == $entity->owner_guid) {
-			      // user is the owner of the entity being commented on
-			      continue;
+					// user is the owner of the entity being commented on
+					continue;
 			    }
-			    
-        $notify_settings_link = elgg_get_site_url() . "notifications/personal/{$user->username}";
+				
+        		$notify_settings_link = elgg_get_site_url() . "notifications/personal/{$user->username}";
 				
 				// Results for a user are...
 				$result[$user->guid] = array();
@@ -72,11 +82,28 @@ function comment_tracker_notify($annotation, $ann_user, $params = array()) {
 						case 'sms':
 						case 'site':
 						case 'web':
-							$message =  sprintf(elgg_echo("comment:notify:{$group_lang}body:web"), $user->name, '<a href="' . $entity->getUrl() . '">' . $entity->title . '</a>', '<a href="' . $ann_user->getUrl() . '">' . $ann_user->name . '</a>',  $annotation->value, $entity_link, $notify_settings_link);
+							$message = elgg_echo("comment:notify:{$group_lang}body:web", array(
+								$user->name,
+								$entity_link,
+								$commenter_link,
+								$annotation->value,
+								$entity->getUrl(),
+								$notify_settings_link,
+								$notify_unsubscribe_link
+							));
 							break;
 						case 'email':
 						default:
-							$message =  sprintf(elgg_echo("comment:notify:{$group_lang}body:email:{$CONFIG->email_content_type}"), $user->name, '<a href="' . $entity->getUrl() . '">' . $entity->title . '</a>', '<a href="' . $ann_user->getUrl() . '">' . $ann_user->name . '</a>',  $annotation->value, $entity_link, $CONFIG->sitename, $notify_settings_link);
+							$message = elgg_echo("comment:notify:{$group_lang}body:email:{$CONFIG->email_content_type}", array(
+								$user->name,
+								$entity_link,
+								$commenter_link,
+								$annotation->value,
+								$entity->getUrl(),
+								$CONFIG->sitename,
+								$notify_settings_link,
+								$notify_unsubscribe_link
+							));
 							if (empty($group_lang)) {
 								$from = $CONFIG->site;
 							}
@@ -115,7 +142,9 @@ function comment_tracker_notify($annotation, $ann_user, $params = array()) {
 	return false;
 }
 
-
+/**
+ * Subscribe user to notifications
+ */
 function comment_tracker_subscribe($user_guid, $entity_guid) {
 	if (elgg_is_logged_in())	{
 		if (empty($user_guid)) {
@@ -133,8 +162,9 @@ function comment_tracker_subscribe($user_guid, $entity_guid) {
 	return false;
 }
 
-
-
+/**
+ * Unsubscribe user from notifications
+ */
 function comment_tracker_unsubscribe($user_guid, $entity_guid) {
 	if (elgg_is_logged_in()) {
 		if (empty($user_guid)) {
@@ -152,10 +182,11 @@ function comment_tracker_unsubscribe($user_guid, $entity_guid) {
 	return false;
 }
 
-
-
+/**
+ * Rename typo in plugin setting name
+ */
 function comment_tracker_update_20121025a() {
-  $plugin_settings = elgg_get_plugin_from_id('comment_tracker');
-  $plugin_settings->allow_comment_notification = $plugin_settings->allow_commnet_notification;
-  $plugin_settings->save();
+	$plugin_settings = elgg_get_plugin_from_id('comment_tracker');
+	$plugin_settings->allow_comment_notification = $plugin_settings->allow_commnet_notification;
+	$plugin_settings->save();
 }
