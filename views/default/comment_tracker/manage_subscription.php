7b5565844628b0d8ed0ac80a027811d75b7c2713
Matt Beckett <beck24@gmail.com>
2016-01-24 16:32:37 -0800

    make subscribe button respect owner notification setting

--- views/default/comment_tracker/manage_subscription.php
+++ views/default/comment_tracker/manage_subscription.php
@@ -19,6 +19,12 @@ if (elgg_instanceof($entity, 'object', 'groupforumtopic') && $entity->status == 
 $subscription_subtypes = comment_tracker_get_entity_subtypes();
 $show_button = elgg_get_plugin_setting('show_button', 'comment_tracker');
 
+if (elgg_get_logged_in_user_guid() == $entity->owner_guid) {
+	if (elgg_get_plugin_setting('notify_owner', 'comment_tracker') != 'yes') {
+		return;
+	}
+}
+
 if ($show_button == 'yes' && in_array($entity->getSubtype(), $subscription_subtypes)) {
 	  $text = '<span data-guid="' . $entity->guid . '">';
 	  if (comment_tracker_is_subscribed(elgg_get_logged_in_user_entity(), $entity)) {
